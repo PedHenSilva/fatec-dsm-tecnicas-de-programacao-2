ecc0063c0b34e33b9436434d20a9b76d1174973d
Pedro Henrique Oliveira Silva <pedro.silva492@fatec.sp.gov.br>
2026-08-28 12:29:10 -0300

    Conclusão Projeto 3

---
+++ projeto-03-pedidos-mcdonalds/McDonalds.js
@@ -0,0 +1,77 @@
+class McOferta {
+    constructor(numeroPedido, numero, lanche, batataTamanho, refrigeranteTamanho) {
+        this.numeroPedido = numeroPedido;
+        this.numero = numero;
+        this.lanche = lanche;
+        this.batataTamanho = batataTamanho;
+        this.refrigeranteTamanho = refrigeranteTamanho;
+    }
+
+    clone() {
+        return new McOferta(
+            this.numeroPedido,
+            this.numero,
+            this.lanche,
+            this.batataTamanho,
+            this.refrigeranteTamanho
+        );
+    }
+}
+
+class GerenciaMcOferta {
+    constructor() {
+        this.originais = {};
+    }
+
+    // Abstração no atributo batata e refrigerante por serem tamanho médio por padrão
+    addMcOferta(numero, lanche, batataTamanho = 'Média', refrigeranteTamanho = 'Médio') {
+        const original = new McOferta(0, numero, lanche, batataTamanho, refrigeranteTamanho);
+        this.originais[numero] = original; 
+    }
+
+    getOfertaByNum(numero) {
+        const ofertaOriginal = this.originais[numero];
+        if (ofertaOriginal) {
+            return ofertaOriginal.clone();
+        }
+        return null;
+    }
+}
+
+const gerencia = new GerenciaMcOferta();
+
+gerencia.addMcOferta(1, 'Big Mac');
+gerencia.addMcOferta(2, 'Quarteirão com Queijo');
+gerencia.addMcOferta(3, 'McChicken');
+gerencia.addMcOferta(4, 'Cheddar McMelt');
+gerencia.addMcOferta(5, 'Quarteirão Duplo com Queijo');
+
+const pedido1 = gerencia.getOfertaByNum(1);
+if (pedido1) {
+    pedido1.numeroPedido = '2203'
+    pedido1.batataTamanho = 'Grande';
+    pedido1.refrigeranteTamanho = 'Grande';
+}
+
+const pedido2 = gerencia.getOfertaByNum(2);
+if (pedido2) {
+    pedido2.numeroPedido = '2204'
+    pedido2.batataTamanho = 'Pequena';
+    pedido2.refrigeranteTamanho = 'Pequeno';
+}
+
+const pedido3 = gerencia.getOfertaByNum(4);
+if (pedido3) {
+    pedido3.numeroPedido = '2205'
+    pedido3.batataTamanho = 'Grande';
+}
+
+console.log("====== McOfertas Originais =======");
+for (let i = 1; i <= 5; i++) {
+    console.log(gerencia.originais[i]); 
+}
+
+console.log("\n======== Pedidos Personalizados =======");
+console.log(pedido1);
+console.log(pedido2);
+console.log(pedido3);
